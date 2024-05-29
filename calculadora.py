84084cb8827b6f978336eb5f0366e0b0830fe24c
RogerioW07 <164174037+RogerioW07@users.noreply.github.com>
2024-05-29 08:12:47 -0300

    primeiro commit

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+ def soma(a, b):
+ returna + b
+   
+ if__name__ == "__main__":
+ num1 = float(input("Digite o primeiro número: "))
+ num2 = float(input("Digite o segundo número: "))
+ resultado = soma(num1, num2)
+ print(f"Resultado: {resultado}"
